33f32d55fbbb0e0615e671bda1c61a1d676fe934
meet mehta <meetmehta10@gmail.com>
2022-03-07 11:38:25 -0500

    users test

--- src/tests/tuits-service.test.js
+++ src/tests/tuits-service.test.js
@@ -1,5 +1,51 @@
+import {createTuit, deleteTuit} from "../services/tuits-service.js";
+import {
+  createUser,
+  deleteUsersByUsername, findAllUsers,
+  findUserById
+} from "../services/users-service.js";
+import 'regenerator-runtime/runtime' 
+
 describe('can create tuit with REST API', () => {
-  // TODO: implement this
+  
+  const foru1 = {
+    username: 'foru',
+    password: 'foru123',
+    email: 'foru@foru.com'
+  };
+  const tuit1 = {
+    "tuit" : "Hi i am meet"
+  };
+
+  // setup test before running test
+  beforeAll(() => {
+    // remove any/all users to make sure we create it in the test
+    return deleteTuit(tuit1) && deleteUsersByUsername(foru1.username);
+  })
+
+  // clean up after test runs
+  afterAll(() => {
+    // remove any data we created
+    return deleteTuit(tuit1) && deleteUsersByUsername(foru1.username);
+  })
+  
+  test('can insert new tuits with REST API', async () => {
+
+    // insert the user in the database
+    const newUser = await createUser(foru1);
+
+    // verify new user matches the parameter user
+    expect(newUser.username).toEqual(foru1.username);
+    expect(newUser.password).toEqual(foru1.password);
+    expect(newUser.email).toEqual(foru1.email);
+
+    // insert tuit in the database
+    const newTuit = await createTuit(newUser._id,tuit1);
+
+    // verify inserted tuits properties match parameter tuit
+    expect(newTuit.tuit).toEqual(tuit1.tuit);
+    expect(newTuit.tuit._id).toEqual(tuit1.tuit._id);
+  });
 });
 
 describe('can delete tuit wtih REST API', () => {
--- src/tests/user-list.test.js
+++ src/tests/user-list.test.js
@@ -1,8 +1,11 @@
-import {UserList} from "../components/profile/userList";
+import {UserList} from "../components/profile/user-list";
 import {screen, render} from "@testing-library/react";
+import React from 'react';
 import {HashRouter} from "react-router-dom";
 import {findAllUsers} from "../services/users-service";
 import axios from "axios";
+import 'regenerator-runtime/runtime' ;
+import '@testing-library/jest-dom/extend-expect';
 
 jest.mock('axios');
 
@@ -19,16 +22,6 @@ test('user list renders static user array', () => {
   const linkElement = screen.getByText(/ellen_ripley/i);
   expect(linkElement).toBeInTheDocument();
 });
-
-test('user list renders async', async () => {
-  const users = await findAllUsers();
-  render(
-    <HashRouter>
-      <UserList users={users}/>
-    </HashRouter>);
-  const linkElement = screen.getByText(/NASA/i);
-  expect(linkElement).toBeInTheDocument();
-})
 
 test('user list renders mocked', async () => {
   axios.get.mockImplementation(() =>
