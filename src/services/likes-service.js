1548dc482842b9e36a55f363463993f8928e339f
meet mehta <meetmehta10@gmail.com>
2022-03-28 00:51:24 -0400

    bug

--- src/services/likes-service.js
+++ src/services/likes-service.js
@@ -8,38 +8,34 @@ const api = axios.create({
   withCredentials: true
 });
 
-const api = axios.create({
-    withCredentials: true
-  });
-  
-  export const findAllTuitsLikedByUser = (userId) =>
-      api.get(`${USERS_API}/${userId}/likes`)
-          .then(response => response.data);
-  
-  export const findAllUsersThatLikedTuit = (tid) =>
-      api.get(`${TUITS_API}/${tid}/likes`)
-          .then(response => response.data);
-  
-  export const userLikesTuit = (uid, tid) =>
-      api.put(`${USERS_API}/${uid}/likes/${tid}`)
-          .then(response => response.data);
-  
-  export const tuitLikedByMe = (uid, tid) =>
-      api.get(`${USERS_API}/${uid}/likes/${tid}`)
-          .then(response => response.data);
-  
-  export const findAllTuitsDislikedByUser = (userId) =>
-      api.get(`${USERS_API}/${userId}/dislikes`)
-          .then(response => response.data);
-  
-  export const findAllUsersThatDislikedTuit = (tid) =>
-      api.get(`${TUITS_API}/${tid}/dislikes`)
-          .then(response => response.data);
-  
-  export const userDislikesTuit = (uid, tid) =>
-      api.put(`${USERS_API}/${uid}/dislikes/${tid}`)
-          .then(response => response.data);
-  
-  export const tuitDislikedByMe = (uid, tid) =>
-      api.get(`${USERS_API}/${uid}/dislikes/${tid}`)
-          .then(response => response.data);
+export const findAllTuitsLikedByUser = (userId) =>
+    api.get(`${USERS_API}/${userId}/likes`)
+        .then(response => response.data);
+
+export const findAllUsersThatLikedTuit = (tid) =>
+    api.get(`${TUITS_API}/${tid}/likes`)
+        .then(response => response.data);
+
+export const userLikesTuit = (uid, tid) =>
+    api.put(`${USERS_API}/${uid}/likes/${tid}`)
+        .then(response => response.data);
+
+export const tuitLikedByMe = (uid, tid) =>
+    api.get(`${USERS_API}/${uid}/likes/${tid}`)
+        .then(response => response.data);
+
+export const findAllTuitsDislikedByUser = (userId) =>
+    api.get(`${USERS_API}/${userId}/dislikes`)
+        .then(response => response.data);
+
+export const findAllUsersThatDislikedTuit = (tid) =>
+    api.get(`${TUITS_API}/${tid}/dislikes`)
+        .then(response => response.data);
+
+export const userDislikesTuit = (uid, tid) =>
+    api.put(`${USERS_API}/${uid}/dislikes/${tid}`)
+        .then(response => response.data);
+
+export const tuitDislikedByMe = (uid, tid) =>
+    api.get(`${USERS_API}/${uid}/dislikes/${tid}`)
+        .then(response => response.data);
