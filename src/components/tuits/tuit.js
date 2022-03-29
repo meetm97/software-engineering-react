11997744f5ef8a90a920e7b27a26c28032833404
meet mehta <meetmehta10@gmail.com>
2022-03-29 10:11:07 -0400

    like functionality

--- src/components/tuits/tuit.js
+++ src/components/tuits/tuit.js
@@ -1,65 +1,33 @@
 import React from "react";
-import TuitStats from "./tuit-stats";
-import TuitImage from "./tuit-image";
-import TuitVideo from "./tuit-video";
-import {useNavigate, Link} from "react-router-dom";
 
-const Tuit = ({tuit, deleteTuit, likeTuit}) => {
-    const navigate = useNavigate();
-    const daysOld = (tuit) => {
-        const now = new Date();
-        const nowMillis = now.getTime();
-        const posted = new Date(tuit.postedOn);
-        const postedMillis = posted.getTime();
-        const oldMillis = nowMillis - postedMillis;
-        let old = 0.0;
-        const secondsOld = oldMillis/1000.0;
-        const minutesOld = secondsOld/60.0;
-        const hoursOld = minutesOld/60.0;
-        const daysOld = hoursOld/24.0;
-        if(daysOld > 1) {
-            old = Math.round(daysOld) + 'd';
-        } else if(hoursOld > 1) {
-            old = Math.round(hoursOld) + 'h';
-        } else if(minutesOld > 1) {
-            old = Math.round(minutesOld) + 'm';
-        } else if(secondsOld > 1) {
-            old = Math.round(secondsOld) + 's';
-        }
-        return old;
-    }
-  return(
-    // <li onClick={() => navigate(`/tuit/${tuit._id}`)}
-    <li className="p-2 ttr-tuit list-group-item d-flex rounded-0">
-      <div className="pe-2">
-        {
-          tuit.postedBy &&
-          <img src={`../images/${tuit.postedBy.username}.jpg`}
-               className="ttr-tuit-avatar-logo rounded-circle"/>
-        }
+const TuitStats = ({tuit, likeTuit = () => {}}) => {
+    return (
+      <div className="row mt-2">
+        <div className="col">
+          <i className="far fa-message me-1"></i>
+          {tuit.stats && tuit.stats.replies}
+        </div>
+        <div className="col">
+          <i className="far fa-retweet me-1"></i>
+          {tuit.stats && tuit.stats.retuits}
+        </div>
+        <div className="col">
+          <span onClick={() => likeTuit(tuit)}>
+              {
+                tuit.stats && tuit.stats.likes && tuit.stats.likes > 0 &&
+                  <i className="fas fa-heart me-1" style={{color: 'red'}}></i>
+              }
+              {
+                tuit.stats && tuit.stats.likes && tuit.stats.likes <= 0 &&
+                  <i className="far fa-heart me-1"></i>
+              }
+            {tuit.stats && tuit.stats.likes}
+          </span>
+        </div>
+        <div className="col">
+          <i className="far fa-inbox-out"></i>
+        </div>
       </div>
-      <div className="w-100">
-          <i onClick={() => deleteTuit(tuit._id)} className="fas fa-remove fa-2x fa-pull-right"></i>
-          <Link to={`/tuit/${tuit._id}`}>
-          <i className="float-end fas fa-circle-ellipsis me-1"></i>
-          </Link>
-        <h2
-          className="fs-5">
-          {tuit.postedBy && tuit.postedBy.username}
-          @{tuit.postedBy && tuit.postedBy.username} -
-            <span className="ms-1">{daysOld(tuit)}</span></h2>
-        {tuit.tuit}
-        {
-          tuit.youtube &&
-            <TuitVideo tuit={tuit}/>
-        }
-        {
-          tuit.image &&
-          <TuitImage tuit={tuit}/>
-        }
-        <TuitStats tuit={tuit} likeTuit={likeTuit}/>
-      </div>
-    </li>
-  );
+    );
 }
-export default Tuit;
+export default TuitStats;
