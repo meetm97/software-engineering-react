a2995e3c9731907c499a637d34e287dab71a4aa4
meet mehta <meetmehta10@gmail.com>
2022-04-01 18:33:31 -0400

    thumps up button fixed

--- src/components/tuits/tuit-stats.js
+++ src/components/tuits/tuit-stats.js
@@ -1,44 +1,29 @@
+
+   
 import React from "react";
 
-const TuitStats = ({tuit, likeTuit, dislikeTuit = () => {}}) => {
+const TuitStats = ({tuit, likeTuit = () => {}}) => {
     return (
       <div className="row mt-2">
         <div className="col">
           <i className="far fa-message me-1"></i>
-          {tuit.stats &&
-          <span className="ttr-stats-replies">{tuit.stats.replies}</span>
-          }
+          {tuit.stats && tuit.stats.replies}
         </div>
         <div className="col">
           <i className="far fa-retweet me-1"></i>
-          {tuit.stats &&
-          <span className="ttr-stats-retuits">{tuit.stats.retuits}</span>
-          }
+          {tuit.stats && tuit.stats.retuits}
         </div>
         <div className="col">
-          <span className="ttr-like-tuit-click" onClick={() => likeTuit(tuit)}>
+          <span onClick={() => likeTuit(tuit)}>
               {
                 tuit.stats && tuit.stats.likes && tuit.stats.likes > 0 &&
-                  <i className="fa-solid fa-thumbs-up me-1" style={{color: 'red'}}></i>
+                  <i className="fas fa-heart me-1" style={{color: 'red'}}></i>
               }
               {
                 tuit.stats && tuit.stats.likes && tuit.stats.likes <= 0 &&
-                  <i className="fa-light fa-thumbs-up me-1"></i>
+                  <i className="far fa-heart me-1"></i>
               }
-            <span className="ttr-stats-likes">{tuit.stats && tuit.stats.likes}</span>
-          </span>
-        </div>
-        <div className="col">
-          <span className="ttr-like-tuit-click" onClick={() => dislikeTuit(tuit)}>
-              {
-                tuit.stats && tuit.stats.dislikes && tuit.stats.dislikes > 0 &&
-                  <i className="fa-solid fa-thumbs-down me-1" style={{color: 'blue'}}></i>
-              }
-              {
-                tuit.stats && tuit.stats.dislikes && tuit.stats.dislikes <= 0 &&
-                  <i className="fa-light fa-thumbs-down me-1"></i>
-              }
-            <span className="ttr-stats-unlikes">{tuit.stats && tuit.stats.dislikes}</span>
+            {tuit.stats && tuit.stats.likes}
           </span>
         </div>
         <div className="col">
