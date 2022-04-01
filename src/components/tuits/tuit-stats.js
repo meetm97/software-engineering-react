49342ad1ba2837a29b2719879dee2a60c978ae8a
meet mehta <meetmehta10@gmail.com>
2022-04-01 19:19:48 -0400

    bug fix for dislike display

--- src/components/tuits/tuit-stats.js
+++ src/components/tuits/tuit-stats.js
@@ -1,52 +1,48 @@
+
+   
 import React from "react";
 
 const TuitStats = ({tuit, likeTuit, dislikeTuit = () => {}}) => {
     return (
-      <div className="row mt-2">
-        <div className="col">
-          <i className="far fa-message me-1"></i>
-          {tuit.stats &&
-          <span className="ttr-stats-replies">{tuit.stats.replies}</span>
-          }
-        </div>
-        <div className="col">
-          <i className="far fa-retweet me-1"></i>
-          {tuit.stats &&
-          <span className="ttr-stats-retuits">{tuit.stats.retuits}</span>
-          }
-        </div>
-        <div className="col">
-          <span className="ttr-like-tuit-click" onClick={() => likeTuit(tuit)}>
+        <div className="row mt-2">
+            <div className="col">
+                <i className="far fa-message me-1"></i>
+                {tuit.stats && tuit.stats.replies}
+            </div>
+            <div className="col">
+                <i className="far fa-retweet me-1"></i>
+                {tuit.stats && tuit.stats.retuits}
+            </div>
+            <div className="col">
+          <span onClick={() => likeTuit(tuit)}>
               {
-                tuit.stats && tuit.stats.likes > 0 &&
-                  <i className="fa-solid fa-thumbs-up me-1" style={{color: 'red'}}></i>
+                  tuit.stats && tuit.stats.likes && tuit.stats.likes > 0 &&
+                  <i className="fa-solid fa-thumbs-up" style={{color: 'red'}}></i>
               }
               {
-                tuit.stats && tuit.stats.likes <= 0 &&
-                  <i className="fa-light fa-thumbs-up me-1"></i>
+                  tuit.stats && tuit.stats.likes && tuit.stats.likes <= 0 &&
+                  <i className="fa-solid fa-thumbs-up"></i>
               }
-            <span className="ttr-stats-likes">{tuit.stats && tuit.stats.likes}</span>
+              {tuit.stats && tuit.stats.likes}
           </span>
         </div>
-        
-        <div className="col">
-          <span className="col" onClick={() => dislikeTuit(tuit)}>
+            <div className="col">
+          <span onClick={() => dislikeTuit(tuit)}>
               {
-                tuit.stats && tuit.stats.dislikes > 0 &&
-                  <i className="fa-solid fa-thumbs-down me-1" style={{color: 'blue'}}></i>
+                  tuit.stats && tuit.stats.dislikes && tuit.stats.dislikes > 0 &&
+                  <i className="fa-solid fa-thumbs-down" style={{color: 'blue'}}></i>
               }
               {
-                tuit.stats && tuit.stats.dislikes <= 0 &&
-                  <i className="fa-light fa-thumbs-down me-1"></i>
+                  tuit.stats && tuit.stats.dislikes && tuit.stats.dislikes <= 0 &&
+                  <i className="fa-solid fa-thumbs-down"></i>
               }
-            <span className="col">{tuit.stats && tuit.stats.dislikes}
-            </span>
+              {tuit.stats && tuit.stats.dislikes}
           </span>
+            </div>
+            <div className="col">
+                <i className="far fa-inbox-out"></i>
+            </div>
         </div>
-        <div className="col">
-          <i className="far fa-inbox-out"></i>
-        </div>
-    </div>
     );
 }
 export default TuitStats;
