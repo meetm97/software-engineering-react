9c90d60390afd792e84111aa353681ee40322e78
meet mehta <meetmehta10@gmail.com>
2022-04-01 14:46:16 -0400

    likes service added

---
+++ src/services/likes-service.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:4000";
+//const BASE_URL = "https://software-engineering-node-dev4.herokuapp.com";
+const USERS_API = `${BASE_URL}/api/users`;
+const TUITS_API = `${BASE_URL}/api/tuits`;
+
+const api = axios.create({
+  withCredentials: true
+});
+
+export const findAllTuitsLikedByUser = (userId) =>
+    api.get(`${USERS_API}/${userId}/likes`)
+        .then(response => response.data);
+
+export const findAllUsersThatLikedTuit = (tid) =>
+    api.get(`${TUITS_API}/${tid}/likes`)
+        .then(response => response.data);
+
+export const userLikesTuit = (uid, tid) =>
+    api.put(`${USERS_API}/${uid}/likes/${tid}`)
+        .then(response => response.data);
+
+export const tuitLikedByMe = (uid, tid) =>
+    api.get(`${USERS_API}/${uid}/likes/${tid}`)
+        .then(response => response.data);
