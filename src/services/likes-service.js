abc5a0aea8727b3014c7e70a969e346712be146f
meet mehta <meetmehta10@gmail.com>
2022-04-02 12:19:48 -0400

    my-likes screen

--- src/services/likes-service.js
+++ src/services/likes-service.js
@@ -13,10 +13,6 @@ export const findAllTuitsLikedByUser = (userId) =>
     api.get(`${USERS_API}/${userId}/likes`)
         .then(response => response.data);
 
-export const findAllUsersThatLikedTuit = (tid) =>
-    api.get(`${TUITS_API}/${tid}/likes`)
-        .then(response => response.data);
-
 export const userLikesTuit = (uid, tid) =>
     api.put(`${USERS_API}/${uid}/likes/${tid}`)
         .then(response => response.data);
